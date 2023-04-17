d0c9ec76da36122cd85c8f06373fc2b19acafa07
Svetlana <medjog@gmail.com>
2023-04-17 21:25:49 +0400

    Решение задачи Num01, рабочая программа

--- Num01/Program.cs
+++ Num01/Program.cs
@@ -7,3 +7,53 @@
 7 4 2 1
 9 5 3 2
 8 4 4 2 */
+
+int[,] Array = new int[5,5];
+FillArray(Array);
+PrintArray(Array);
+SortRowsArrayMaxMin(Array);
+Console.WriteLine();
+PrintArray(Array);
+
+void FillArray(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i,j] = new Random().Next(1,10);
+            }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i,j]} ");
+            }
+    Console.WriteLine();        
+    }
+}
+void SortRowsArrayMaxMin(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int count = 0; count < array.GetLength(1) - 2 ; count++ )
+            {
+                for (int j = 0; j < array.GetLength(1) - 1 ; j++)
+                    {
+                        if (array[i,j] < array[i,j+1])
+                        {
+                        int basket = array[i,j];
+                        array[i,j] = array[i,j+1];
+                        array[i,j+1] = basket;
+                        }
+
+                    }
+            }
+    }
+}
+
+
