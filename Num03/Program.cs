35d1044d59057c380df05e51c6359f7f2262c954
Svetlana <medjog@gmail.com>
2023-04-20 20:07:00 +0400

    Рабочая версия Num03 для матриц 2х2, 3х3...

--- Num03/Program.cs
+++ Num03/Program.cs
@@ -6,4 +6,58 @@
 18 20
 15 18  */
 
+int[,] Matrix1 = new int[3,3];
+int[,] Matrix2 = new int[3,3];
+FillArray(Matrix1);
+FillArray(Matrix2);
 
+PrintArray(Matrix1);
+Console.WriteLine();
+PrintArray(Matrix2);
+MatrixMultiplication(Matrix1, Matrix2);
+Console.WriteLine();
+PrintArray(MatrixMultiplication(Matrix1, Matrix2));
+
+void FillArray(int[,] array)
+{
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i,j] = new Random().Next(1,5);
+            }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i,j]} ");
+            }
+    Console.WriteLine();        
+    }
+}
+
+int[,] MatrixMultiplication(int[,] matrix1, int[,]matrix2)
+{   int I = 0;
+    int j = 0;
+    int[,] MatrixMult = new int[3,3];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+        //Console.WriteLine($"строка первой матрицы {i}");
+        for (int J = 0; J < matrix2.GetLength(1); J++)
+            {
+           // Console.WriteLine($"столбец второй матрицы {j}");
+            int result = matrix1[i,j] * matrix2[I,J] + matrix1[i,j+1] * matrix2[I+1,J];
+            //Console.WriteLine($"вот и он {result} ");
+            MatrixMult[i,J] = result;
+            }
+
+        }
+        return MatrixMult;
+    }
+
+
+   
