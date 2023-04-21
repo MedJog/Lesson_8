352aa6d3690d7bb59ab2eceaa5edf0b420a7de6b
Svetlana <medjog@gmail.com>
2023-04-21 16:23:01 +0400

    Num03 не только для квадратных матриц + консольный ввод и проверка условия умножения матриц

--- Num03/Program.cs
+++ Num03/Program.cs
@@ -6,6 +6,9 @@
 18 20
 15 18  */
 
+
+// Только для квадратных матриц
+
 int[,] Matrix1 = new int[3,3];
 int[,] Matrix2 = new int[3,3];
 FillArray(Matrix1);
@@ -18,6 +21,34 @@ MatrixMultiplication(Matrix1, Matrix2);
 Console.WriteLine("Результирующая матрица");
 PrintArray(MatrixMultiplication(Matrix1, Matrix2));
 
+// Для всех матриц
+
+Console.WriteLine("Введите количество строк матрицы-1: ");
+int rows1=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы-1: ");
+int columns1=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк матрицы-2: ");
+int rows2=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы-2: ");
+int columns2=int.Parse(Console.ReadLine());
+int[,] Matr1 = FillArray2(rows1, columns1);
+int[,] Matr2 = FillArray2(rows2, columns2);
+
+Console.WriteLine("Первая матрица");
+PrintArray(Matr1);
+Console.WriteLine("Вторая матрица");
+PrintArray(Matr2);
+if (columns1 != rows2)
+{
+    Console.WriteLine("Умножение данных матриц выполнить невозможно.");
+}
+else
+{
+    Console.WriteLine("Результирующая матрица");
+    PrintArray(MatrixMultiplication2(Matr1, Matr2));
+} 
+
+
 void FillArray(int[,] array)
 {
  for (int i = 0; i < array.GetLength(0); i++)
@@ -28,6 +59,20 @@ void FillArray(int[,] array)
             }
     }
 }
+
+int[,] FillArray2(int rows, int columns)
+{
+int[,] matrix = new int[rows,columns];
+ for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = new Random().Next(1,5);
+            }
+    }
+return matrix;
+}
+
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -56,6 +101,23 @@ int[,] MatrixMultiplication(int[,] matrix1, int[,]matrix2)
         }
         return MatrixMult;
 }    
+
+ int[,] MatrixMultiplication2(int[,] matrix1, int[,]matrix2)
+{   
+    int I = 0;
+    int j = 0;
+    int[,] MatrixMult = new int[rows1,columns2];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+        for (int J = 0; J < matrix2.GetLength(1); J++)
+            {
+            int result = matrix1[i,j] * matrix2[I,J] + matrix1[i,j+1] * matrix2[I+1,J];
+            MatrixMult[i,J] = result;
+            }
+
+        }
+        return MatrixMult;
+}   
 
 
 
