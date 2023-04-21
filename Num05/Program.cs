fad7d8c75bcda5320bfe239f35e7056835e60254
Svetlana <medjog@gmail.com>
2023-04-21 12:43:52 +0400

    Num05 только для массива 4х4

--- Num05/Program.cs
+++ Num05/Program.cs
@@ -7,25 +7,11 @@
 
 int[,] Array = new int[4,4];
 
-FillArray(Array);
-PrintArray(Array);
-Console.WriteLine();
+
 HelixArray(Array, 0, 0);
 PrintArray(Array);
 Console.WriteLine();
-//HelixArray(Array, 1, 1); 
-//PrintArray(Array);
 
-void FillArray(int[,] array)
-{
- for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            {
-                array[i,j] = 0;
-            }
-    }
-}
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -36,7 +22,7 @@ void PrintArray(int[,] array)
             }
     Console.WriteLine();        
     }
-}
+} 
 
 
 int[,] HelixArray(int[,]array, int m, int n)
@@ -52,25 +38,17 @@ int[,] HelixArray(int[,]array, int m, int n)
             Array[i,j] = element + 1;
             element++; 
         }
-Console.WriteLine($"i = {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
     for (i = i+1; i < M; i++)
         {
         Array[i,j-1] = element + 1;
         element++; 
         }
-Console.WriteLine($"i = {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
+
     for (j = j-1; j > 0; j--)
         {
         Array[i-1,j-1] = element + 1;
         element++; 
         }
-Console.WriteLine($"i = {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
     for (i = i - 2; i > 0; i--)
         {
         Array[i,j] = element + 1;
@@ -78,37 +56,20 @@ Console.WriteLine($"элемент {element}");
         }
     M = M - 1;
     N = N - 1;
-    
-Console.WriteLine($"i =  {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
-Console.WriteLine($"M =  {M}");
-Console.WriteLine($"N =  {N}");
     for (j = n+1; j < N; j++)
         {
             Array[i+1,j] = element + 1;
             element++; 
         }
-Console.WriteLine($"i = {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
     for (i = i+2; i < M; i++)
         {
         Array[i,j-1] = element + 1;
         element++; 
         }
-Console.WriteLine($"i = {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
     for (j = j-1; j > 1; j--)
         {
         Array[i-1,j-1] = element + 1;
         element++; 
         }
-Console.WriteLine($"i = {i}");
-Console.WriteLine($"j = {j}");
-Console.WriteLine($"элемент {element}");
-
-
 return array;
 }
