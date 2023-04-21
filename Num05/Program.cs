a1a65a30e61c68def29ffe365e11f4c23888e4ec
Svetlana <medjog@gmail.com>
2023-04-21 11:57:28 +0400

    Num05 заполняет массив

--- Num05/Program.cs
+++ Num05/Program.cs
@@ -12,6 +12,10 @@ PrintArray(Array);
 Console.WriteLine();
 HelixArray(Array, 0, 0);
 PrintArray(Array);
+Console.WriteLine();
+//HelixArray(Array, 1, 1); 
+//PrintArray(Array);
+
 void FillArray(int[,] array)
 {
  for (int i = 0; i < array.GetLength(0); i++)
@@ -28,36 +32,83 @@ void PrintArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
             {
-                Console.Write($"{array[i,j]} ");
+                Console.Write($"{array[i,j]}   ");
             }
     Console.WriteLine();        
     }
 }
 
 
-void HelixArray(int[,]array, int i, int j)
-{
+int[,] HelixArray(int[,]array, int m, int n)
+{   
+    int i = m; // индекс строки
+    int j = n; // индекс столбца
+
+    int M = Array.GetLength(0); // строки массива
+    int N = Array.GetLength(1); // столбцы массива
     int element = 0;
-    for (j = 0; j < Array.GetLength(1); j++)
+    for (j = n; j < N; j++)
         {
             Array[i,j] = element + 1;
             element++; 
         }
-
-for (i = i+1; i < Array.GetLength(0); i++)
-    {
+Console.WriteLine($"i = {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
+    for (i = i+1; i < M; i++)
+        {
         Array[i,j-1] = element + 1;
         element++; 
-    }
-for (j = j-1; j > 0; j--)
-    {
+        }
+Console.WriteLine($"i = {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
+    for (j = j-1; j > 0; j--)
+        {
         Array[i-1,j-1] = element + 1;
         element++; 
-    }
-for (i = i - 2; i > 0; i--)
-    {
+        }
+Console.WriteLine($"i = {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
+    for (i = i - 2; i > 0; i--)
+        {
         Array[i,j] = element + 1;
         element++; 
-    }
+        }
+    M = M - 1;
+    N = N - 1;
+    
+Console.WriteLine($"i =  {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
+Console.WriteLine($"M =  {M}");
+Console.WriteLine($"N =  {N}");
+    for (j = n+1; j < N; j++)
+        {
+            Array[i+1,j] = element + 1;
+            element++; 
+        }
+Console.WriteLine($"i = {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
+    for (i = i+2; i < M; i++)
+        {
+        Array[i,j-1] = element + 1;
+        element++; 
+        }
+Console.WriteLine($"i = {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
+    for (j = j-1; j > 1; j--)
+        {
+        Array[i-1,j-1] = element + 1;
+        element++; 
+        }
+Console.WriteLine($"i = {i}");
+Console.WriteLine($"j = {j}");
+Console.WriteLine($"элемент {element}");
 
+
+return array;
 }
